a57edd261813d00e766d5f5865b99504aba98380
Ishraq Ahmed <ishraqahmed@ishraqahmed.net>
2023-06-01 14:54:51 +0100

    30th march 2023 v5.3 1/2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 /**
-* Template Name: MyResume - v4.10.0
+* Template Name: MyResume
+* Updated: May 30 2023 with Bootstrap v5.3.0
 * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
